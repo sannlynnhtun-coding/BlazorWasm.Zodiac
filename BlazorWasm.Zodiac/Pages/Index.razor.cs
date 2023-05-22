8fc1c1c7096be84100e414ce6430a298a5e6f541
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-05-23 00:19:24 +0630

    UI Code Changes

---
+++ BlazorWasm.Zodiac/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorWasm.Zodiac.Pages
+{
+    public partial class Index
+    {
+        private bool _isLoading = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(500);
+            _isLoading = false;
+        }
+    }
+}
